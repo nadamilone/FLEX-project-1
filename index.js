4c6a2a304b580c84de587d5d87afedc45610861a
NADA MILONE <nadamilone@gmail.com>
2022-06-16 11:56:06 -0400

    update: checkbox event listener added

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ fetch("https://api.artic.edu/api/v1/artworks/214960")
   .then((response) => response.json())
   .then((data) => artListDetails(data));
 
-function renderArtArray(arts) {
-  arts.forEach((art) => {
-    const artDetails = document.querySelector("#art-details");
-    const li = document.createElement("li");
-    li.textContent = art.title;
-    li.addEventListener("click", () => artListDetails(art));
-    artDetails.append(li);
-  });
-  debugger;
-  console.log("hi");
-}
+// fetch("")
+//   .then((response) => response.json())
+  // .then((imgData) => console.log(imgData));
+
+
+// function renderArtArray(arts) {
+//   arts.forEach((art) => {
+//     const artDetails = document.querySelector("#art-details");
+//     const li = document.createElement("li");
+//     li.textContent = art.title;
+//     li.addEventListener("click", () => artListDetails(art));
+//     artDetails.append(li);
+//   });
+//   debugger;
+//   console.log("hi");
+// }
 
 function artListDetails(art) {
-  console.log(art.data.title);
+  //   console.log(art.data.title);
   const refNum = document.querySelector("#main_reference_number");
   refNum.textContent = art.data.main_reference_number;
   // const img = document.querySelector("#image_id");
@@ -30,6 +35,7 @@ function artListDetails(art) {
   size.textContent = art.data.dimensions;
   const artist = document.querySelector("#artist_display");
   artist.textContent = art.data.artist_display;
+
   const onLoan = document.querySelector("#on_loan_display");
   onLoan.textContent = art.data.on_loan_display;
   const subject = document.querySelector("#subject_titles");
@@ -55,4 +61,8 @@ likeBtn.addEventListener("click", () => {
 const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
+  console.log("let there be nothing but darkness in your enemies future");
 });
+
+//getElementById for filter nav bar???
+//onClick??
